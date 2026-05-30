991c064cdcc460ec1638f9cc97e07d909dd5d528
unknown <pareekswati42@gmail.com>
2026-05-30 12:06:38 +0530

    data science

---
+++ Python/app.py
@@ -0,0 +1,335 @@
+# print("hello hiii kese ho 😂😂")
+
+
+# name = "Ritik"
+# _name = "dev"
+
+
+
+# last5515151name = ""
+
+
+# 5 = 10 
+
+
+
+# age = 10 
+# Age = 20 
+# AGE = 30 
+# print(age)
+# print(Age)
+# print(AGE)
+
+
+# print(_name)
+
+# @ = 10 
+# $ = 10 
+# % = 20 
+# ^ = 10 
+# & = 0 
+
+########## 
+
+
+# data types in python
+# str 
+# bool 
+# int 
+# # char  
+# float
+# list 
+# tuple 
+# # array 
+# dict 
+# set 
+# complex 
+
+
+# a = 10
+# print(a)
+
+
+# >>>>>>>>string datatype in python
+# name = "upflairs"
+# print("This is my first string :- ",name) 
+# print("type of my first string :- ",type(name))  #### type function data type btata hai 
+# num ="2345"
+
+# print(num)
+# print("type of my first string :- ",type(num))
+
+
+
+
+# name = "upflairs"
+# print("len of my first string👉👉",len(name)) ### find the len of the string 
+
+# indexing & slicing 
+
+# print(name[0])
+# print(name[2])
+# print(name[4])
+# print(name[5])
+# print(name[1])
+
+
+
+# print("Slicing :- ",name[0:4])
+# 
+
+# print(name[-1])
+# print(name[-1:-7])  ### task 1 
+
+# name = "Ritik"
+# print(len(name))
+
+# comapny_name= "upflairs pvt ldt"
+# # lower case convert into upper case 👉👉👉
+# upper_case =comapny_name.upper()
+# print("upper case 🔫🔫🔫🔫",upper_case)
+
+# upper case convert into lower case
+
+# comapny_name="UPFLAIRS PVT LDT"
+# lower_Case =comapny_name.lower()
+
+# print("lower case 👉",lower_Case)
+ 
+# lower_case =comapny_name.casefold() #### task2 
+# print(lower_case)
+
+
+
+
+# company_name = "upflairs pvt ldt"
+# first_latter=company_name.title()
+# print(first_latter)
+
+# first_latter=company_name.capitalize()
+# print("👉👉👉👉",first_latter)
+
+
+# c =name.count('r')
+# print(c)
+
+
+# print(name.index('t'))
+
+
+# name = "ritrrik"
+# last_name = "Kumar"
+
+# print("😒😒😒",name + last_name
+# )
+
+
+# print("🥹🥹🥹",name,last_name
+# )
+
+
+# print("😂😂😂",name +" "+ last_name
+# )
+
+
+
+# name = "ritrrik"
+# last_name = "Kumar"
+
+# print(name+last_name)
+# print(name*last_name)
+# print(name/last_name)
+# print(name//last_name)
+# print(name**last_name)
+# print(name+last_name)
+
+
+# name = "Diya "
+# print(name*10
+#       )
+
+
+
+
+# intro = """Hello everyone,
+# My name is Ritik Kumawat, and I am a developer and Data Science Engineer from Jaipur, Rajasthan. I specialize in Python, Machine Learning, Deep Learning, FastAPI, Flask, and Generative AI. I also work with cloud technologies like AWS and databases such as MySQL and MongoDB.
+
+# Currently, I work as a trainer and developer where I help students learn modern technologies and build real-world projects. I am passionate about teaching, coding, and creating innovative AI applications.
+
+# I believe in continuous learning, practical knowledge, and maintaining good communication while working on projects. Thank you.
+# """
+# print(intro)
+
+
+# "I believe in continuous learning, practical knowledge, and maintaining good communication while working on projects. Thank you.
+# "
+
+
+# name = "Govind"
+# address = "jaipur rajasthan "
+# print(f"my name is {name} and i am from {address} ")
+
+
+# path = r"D:\NOTES IN PDF\PPT"
+
+# path = r"C:\Program Files (x86)\Microsoft Office\Office16"
+# print(path)
+
+
+
+# >>>>>>>>list 
+
+# hetrogenus 
+# lst = [1,2,5,2,5,1, "hello", 3.4 , "😍😍😍😍"]
+
+# print("This is my first list :- ",lst)
+# print("Type of my list :-  ",type(lst))
+# print("len of my list :- ",len(lst))
+
+
+# print("indexing and Slicing :->>>>>")
+
+
+# # list = [1,2,2,5,5]
+# # print(list)
+# lst = [1,2,5,2,5,1, "hello", 3.4 , "😍😍😍😍"]
+# print(lst[0])
+# print(lst[2])
+# print(lst[5])
+# print(lst[3])
+
+# print("slcing >>>>>>>")
+
+# print(lst[0:3])
+# print(lst[2:4])
+# print(lst[5:8])
+# print(lst[1:3])
+
+
+# name ="Ritik" 
+# print("f" in name)
+
+
+# lst = [1,2,5,2,5,1, "hello", 3.4 , "😍😍😍😍"]
+# lst.append("upflairs")
+# lst.insert(0, "upflairs")
+
+# lst.remove()
+# lst.pop(0)
+# lst.extend([1,0])
+# lst.count()
+# lst.clear()
+# lst.copy()
+
+# print(lst)
+
+
+lst1 = [1,2,2,54]
+lst2 = [8,2,5,5]
+# print(lst1+lst2)
+# print(lst1*lst2)
+# print(lst1/lst2)
+# # print(lst1//lst2)
+
+# lst = ["hello", 2,4,54,5]
+# max()      # largest value
+# min()      # smallest value
+# sum()      # total addition
+# len()      # length/count
+
+
+# lst = [1,2,2,4,2,2]
+# # lst.sort()
+# lst.reverse()
+# print(lst)
+# lst[0]="hello"
+# print(4 in lst)
+# print(lst)
+
+
+
+
+
+
+
+
+
+
+
+
+# num , bool , sequence(string , list, tuple  ), dict , set 
+# >>>>>>>>>>>>>>>>>>>>>>>>...Tuple >>>>>>>>>
+
+# tuple = ()
+# A Tuple in Python is an ordered collection of items that is immutable,
+# which means its values cannot be changed after creation. 
+# Tuples are written using round brackets ().
+
+
+# >>>>>>Key Points:
+# Ordered collection
+# Immutable (cannot modify items)
+# Allows duplicate values
+# Can store different data types
+
+
+tpl = (1,2,3,4,5,"hii","Rahul", 4.5, "😍😍😍😒")
+# print("This is my first tuple :- ",tpl)
+# print("Type of my first tuple :- ",type(tpl))
+# print("len of my first tuple :- ",len(tpl))
+
+
+# print(">>>>>>>>>>>>>>>>>>>>>... indexing and Slcing <<<<<<<<<< ")
+# print(tpl[0])
+# print(tpl[1])
+# print(tpl[2])
+# print(tpl[3])
+# print(tpl[4])
+# print(tpl[2:5])
+
+
+
+# tpl.count
+# tpl.index
+
+
+a =1,2,3, "hello", "hii"
+# print(a)
+# print(type(a))
+# print(len(a))
+
+
+# print("<<<<<<<<< Tuple unpacking python >>>>>>>>>")
+# a, b ,c  = (1,2,3)
+# print(a)
+# print(b)
+# print(c)
+
+
+# print("<<<<<<<<< Tuple unpacking python >>>>>>>>>")
+# a, b   = (1,2,3)
+# print(a)
+# print(b)
+# print(c)
+
+
+
+
+
+
+# type casting 
+tpl = (1,2,3,5, "hello", "hiii")
+print("My tuple ",tpl)
+print("Type of my tuple :- ",type(tpl))
+print("<<<<<<<<<<< Tuple convert into list >>>>>>>>>>")
+lst =list(tpl)
+print("This is my list :- ",lst)
+print("Type of my list :- ",type(lst))
+print("------------------>add item in list ")
+lst.append("item add ho chuke ")
+print("list me item add ho chuke hai :- ",lst)
+print("----------------------------------->>>>> list convert into Tuple ")
+tpl =tuple(lst)
+print("My tuple ",tpl)
+print("Type of my tuple :- ",type(tpl))
+print("tuple ke item add ho chuke hai ")
---
+++ Python/basic.py
@@ -0,0 +1,193 @@
+# print("hello hii kese ho ") 
+# data types 
+# 0 data types 
+# name = "upflairs"
+# print(name)
+
+
+# print("type of variable 👉👉👉👉",type(name)) 
+
+
+# str 
+# list 
+# tuple 
+# dict 
+# set 
+# bool 
+# complex 
+# int 
+# float 
+
+
+
+
+#>>>>>>>>>>>>>>>>>>>>>> str 
+name = "Diya"
+# print("My Name is :- ",name)
+
+# print("type of my variable : - ",type(name)) #### type funtion type check krne ke kam aata hai
+
+# print("len of my string : - ",len(name)) 
+# upper_case =name.upper()
+# print("upper case :- ",upper_case)
+# lower_Case =upper_case.lower()
+# print("lower case :- ",lower_Case)
+
+
+# lw =upper_case.casefold()
+
+# print(lw)
+
+# name = "RITIK"
+# print(name.casefold()) task 2 
+# name = "dev"
+# print(name.title())
+# print(name.capitalize()) Task 2 
+
+
+company_name  = "upflairs"
+# print(len(company_name))
+# print(company_name.strip())
+
+
+# intro = " hello hii kese ho" ### task 3 
+
+
+# indexing and slicing 
+company_name  = "upflairs"
+# print(company_name[0])
+# print(company_name[7])
+# print(company_name[-1])
+
+# print(company_name[0:3])
+# company_name >>>> task 4  revrse the string 
+
+# name = "Ritik "
+# last_name = " kumar"
+
+# print(name,last_name)   #### join
+# print(name + " "+ last_name)
+
+
+# str * str 
+# name = "Dev"
+# print(name * name)  #>>>>>>
+# print(name + name)
+# print(name + 2)
+# print(name *  2)
+
+
+# intro ="""Hey, I’m Ritik Kumawat — a passionate developer and Data Science enthusiast from Rajasthan.
+# I work on Python, Machine Learning, Deep Learning,
+# FastAPI, and Generative AI projects.
+# I love building smart applications, exploring new tech,
+# and turning ideas into real-world solutions. 🚀"""
+# print(intro)
+
+# intro.strip
+# intro.split()
+
+# intro ="hello my name is   \n govind "
+# intro.split("\n", "")
+# name = 'dev'
+# name = "dev"   ### task 
+
+# name = "Govind"
+# address = "jaipur"
+# print(f"my name is {name} and i from {address} ")
+# path = r"C:\Users\kumaw\OneDrive\Desktop"
+# print(path)
+
+
+# input function 
+# name =input("enter you name : -")
+# print(name)
+# print(type(name))
+
+# number1  =int(input("enter you first number :- "))
+# number2  =int(input("enter you second number :- "))
+
+# print(number1+ number2)
+# print(type(number1))
+# print(type(number2))
+
+# >>>>>>>>>>>> list >>>>>>>>
+
+
+
+# lst =[1,2,3,5,2,5,"hello", 3.2]
+# print("This is my first list :- ",lst)
+# print("Len of my list :-",len(lst))
+# print("Type of my list :- ",type(lst))
+
+
+# print = 10 
+# print(print)
+# list = 10 
+# print(">>><><<<<<",list)
+# lst =[1,2,3,5,2,5,"hello", 3.2]
+# print(lst[0])
+# print(lst[3])
+# print(lst[2])
+# print(lst[4])
+# print(lst[6])
+# print(lst[7])
+
+# print(lst[-1]
+# )
+
+
+# print(lst[0:5])
+# print(lst[2:5])
+# print(lst[2:7])
+
+
+
+
+# operations 
+
+# fruits = ["apple", "banana", "mango"] 
+# add item
+# fruits.append("orange") ####
+# fruits.insert(0, "grapes")
+# fruits.remove("apple")
+# fruits.remove(2)
+# fruits.pop()
+# fruits.pop(1)
+# fruits.clear()
+# fruits.copy()
+# print(fruits.count("apple"))
+# print(fruits.index("apple"))
+# fruits[0]="Kiwi"
+
+# fruits.reverse()
+# print("fruits list :- ",fruits)
+# lst1 = [1,2,32]
+# lst2 = [ 2,3,5]
+# print(">>>>>>>",2 in lst2)
+# print(lst1+lst2)
+# | Function | Use             |
+# | -------- | --------------- |
+# | len()    | Length          |
+# | max()    | Maximum value   |
+# | min()    | Minimum value   |
+# | sum()    | Sum of elements |
+# | sorted() | Sort list       |
+# 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
